43c0b7cf5fe391e54a417875ef9bd81b180ac170
Роман <romork101104@gmail.com>
2025-11-18 18:43:15 +0700

    dynamic

--- HFind/Source.cpp
+++ HFind/Source.cpp
@@ -24,67 +24,71 @@ int bin_search(vector<T> mas, T search) { // работает правильно
     if (mas[step] == search) return step;
     else return -1;
 }
+
 vector<vector<int>> Graph_Transpose(vector<vector<int>> Graph) { // работает правильно
     vector<vector<int>> Graph_Transposed;
+    //for (int i = 0; i < M; i++)Graph_Transposed.push_back(vector<int>());
+    //int* mas_b = new int[N];
     int max_edge = 0;
     for (int i = 0; i < Graph.size(); i++) if (Graph[i][Graph[i].size() - 1] > max_edge) max_edge = Graph[i][Graph[i].size() - 1];
     for (int i = 0; i <= max_edge; i++)Graph_Transposed.push_back(vector<int>());
     for (int i = 0; i < Graph.size(); i++) for (int j = 0; j < Graph[i].size(); j++) Graph_Transposed[Graph[i][j]].push_back(i);
-    for (int i = 0; i <= Graph_Transposed.size(); i++) sort(Graph_Transposed.begin(), Graph_Transposed.end());
     return Graph_Transposed;
 }
-vector<int> g(vector<vector<int>> Graph, vector<int> input, bool is_vertex = true) { // работает правильно
-    int massize = input.size();
+
+vector<int> g(vector<vector<int>> Graph, vector<int> input, bool is_edges = true) { // работает правильно
     vector<int> ans;
-    vector<int> b;
-    for (int i = 0; i < input.size(); i++)b.push_back(0);
-    if (!is_vertex) {
-        while (true) {
-            bool flag = 1;
-            for (int j = 0; j < input.size(); j++) {
-                if (b[j] == Graph[input[j]].size()) return ans;
-                if (Graph[input[j]][b[j]] > Graph[input[0]][b[0]]) {
-                    b[0]++;
-                    flag = 0;
+    if (is_edges) {
+        for (int i = 0; i < Graph.size(); i++) {
+            if (Graph[i].size() < input.size()) continue;
+            int j = 0, k = 0;
+            while (j < Graph[i].size() && k < input.size()) {
+                if (Graph[i][j] == input[k]) {
+                    j++;
+                    k++;
+                    continue;
                 }
-                if (Graph[input[j]][b[j]] < Graph[input[0]][b[0]]) {
-                    b[j]++;
-                    flag = 0;
+                if (Graph[i][j] < input[k]) {
+                    j++;
+                    continue;
+                }
+                if (Graph[i][j] > input[k]) {
+                    break;
                 }
             }
-            if (flag) {
-                ans.push_back(Graph[input[0]][b[0]]);
-                b[0]++;
-            }
+            if (k == input.size()) ans.push_back(i);
         }
     }
     else {
         auto GraphT = Graph_Transpose(Graph);
-        while (true) {
-            bool flag = 1;
-            for (int j = 0; j < input.size(); j++) {
-                if (b[j] == GraphT[input[j]].size()) return ans;
-                if (GraphT[input[j]][b[j]] > GraphT[input[0]][b[0]]) {
-                    b[0]++;
-                    flag = 0;
+        for (int i = 0; i < GraphT.size(); i++) {
+            if (GraphT[i].size() < input.size()) continue;
+            int j = 0, k = 0;
+            while (j < GraphT[i].size() && k < input.size()) {
+                if (GraphT[i][j] == input[k]) {
+                    j++;
+                    k++;
+                    continue;
                 }
-                if (GraphT[input[j]][b[j]] < GraphT[input[0]][b[0]]) {
-                    b[j]++;
-                    flag = 0;
+                if (GraphT[i][j] < input[k]) {
+                    j++;
+                    continue;
+                }
+                if (GraphT[i][j] > input[k]) {
+                    break;
                 }
             }
-            if (flag) {
-                ans.push_back(GraphT[input[0]][b[0]]);
-                b[0]++;
-            }
+            if (k == input.size()) ans.push_back(i);
         }
     }
     return ans;
 }
-vector<int> gg(vector<vector<int>> Graph, vector<int> input, bool is_vertex = true) { // работает правильно
-    if (is_vertex) return g(Graph, g(Graph, input, true), false);
+
+vector<int> gg(vector<vector<int>> Graph, vector<int> input, bool is_edges = true) { // работает правильно
+    if (is_edges) return g(Graph, g(Graph, input, true), false);
     else return g(Graph, g(Graph, input, false), true);
 }
+
 void Clu(map<vector<int>, vector<int>>& delta_level, vector<vector<int>> Graph, int level_num, int* delta_U) { // работает правильно
     for (int i = 0; i < Graph.size(); i++) {
         if (level_num > delta_U[i]) continue;
@@ -98,13 +102,14 @@ void Clu(map<vector<int>, vector<int>>& delta_level, vector<vector<int>> Graph, 
         while (p >= 0) {
             vector<int> Xu;
             for (int j = 0; j < level_num; j++) Xu.push_back(Graph[i][v[j]]);
-            delta_level[Xu].push_back(i);
+            delta_level[Xu].push_back(i);                                       //Ключ - рёбра(номера столбцов), значение - вершины(номера строк)
             if (v[level_num - 1] == delta_U[i] - 1) p--;
             else p = level_num - 1;
             if (p >= 0) for (int j = level_num - 1; j >= p; j--) v[j] = v[p] + j - p + 1;
         }
     }
 }
+
 void Clu_parallel(map<vector<int>, vector<int>>& delta_level, vector<vector<int>> Graph, int level_num, int* delta_U) {
     map<vector<int>, vector<int>> delta_thread[num_threads];
     
@@ -140,7 +145,8 @@ void Clu_parallel(map<vector<int>, vector<int>>& delta_level, vector<vector<int>
         delta_thread[p].clear();
     }
 }
-void HFindMCS(map<vector<int>, vector<int>>& MCS, vector<vector<int>> Graph,int parallel = 0) { // работает правильно
+
+void HFindMCS(vector<pair<vector<int>, vector<int>>>& MCS, vector<vector<int>> Graph,int parallel = 0) { // работает правильно
     int delta = 0, * delta_U = new int[Graph.size()];
     for (int i = 0; i < Graph.size(); i++) {
         delta_U[i] = Graph[i].size();
@@ -151,14 +157,19 @@ void HFindMCS(map<vector<int>, vector<int>>& MCS, vector<vector<int>> Graph,int 
         if (parallel == 0) Clu(levels[i - 1], Graph, i, delta_U);
         if (parallel == 1) Clu_parallel(levels[i - 1], Graph, i, delta_U);
         for (auto it = levels[i - 1].begin(); it != levels[i - 1].end(); ++it) {
-            if (!MCS.count(it->second)) MCS[it->second] = it->first;
+            bool flag = 1;
+            for (int j = 0; j < MCS.size();j++) {
+                if (MCS[j].second == it->second) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) MCS.push_back(make_pair(it->first, it->second));
         }
     }
-    /*int SIZE = 0;                //Вывод числа комбинаций
-    for (int i = 0; i < delta; i++) SIZE += levels[i].size();
-    cout << SIZE;*/
 }
-void HFindMCS_parallel(map<vector<int>, vector<int>>& MCS, vector<vector<int>> Graph) {
+
+void HFindMCS_parallel(vector<pair<vector<int>, vector<int>>>& MCS, vector<vector<int>> Graph) {
     int delta = 0, * delta_U = new int[Graph.size()];
     for (int i = 0; i < Graph.size(); i++) {
         delta_U[i] = Graph[i].size();
@@ -167,16 +178,18 @@ void HFindMCS_parallel(map<vector<int>, vector<int>>& MCS, vector<vector<int>> G
     map<vector<int>, vector<int>>* levels = new map<vector<int>, vector<int>>[delta];
 #pragma omp parallel for num_threads(num_threads)
     for (int i = delta; i > 0; i--) Clu(levels[i - 1], Graph, i, delta_U);
-    /*
-#pragma omp parallel num_threads(num_threads)
-    {
-        for (int i = delta - omp_get_thread_num(); i > 0; i -= (omp_get_num_threads() + 1)) Clu(levels[i - 1], Graph, i, delta_U);
-    }*/
 #pragma omp barrier
     for (int i = delta; i > 0; i--) {
         //Clu(levels[i - 1], Graph, i, delta_U);
         for (auto it = levels[i - 1].begin(); it != levels[i - 1].end(); ++it) {
-            if (!MCS.count(it->second)) MCS[it->second] = it->first;
+            bool flag = 1;
+            for (int j = 0; j < MCS.size(); j++) {
+                if (MCS[j].second == it->second) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) MCS.push_back(make_pair(it->first, it->second));
         }
     }
     /*int SIZE = 0;                //Вывод числа комбинаций
@@ -184,46 +197,49 @@ void HFindMCS_parallel(map<vector<int>, vector<int>>& MCS, vector<vector<int>> G
     cout << SIZE;*/
 }
 
-/*void Dynamic_MCS(vector<pair<vector<int>, vector<int>>>& MCS, vector<int> new_line, vector<vector<int>> old_graph) {
- //vector<pair<vector<int>, vector<int>>> new_vertex_combinations;
- vector<vector<int>> new_vertex_combinations;
- vector<vector<int>> graph;
- graph.push_back(new_line);
+void Dynamic_MCS(vector<pair<vector<int>, vector<int>>>& MCS, vector<int> new_line, vector<vector<int>> old_graph) {
+    //vector<pair<vector<int>, vector<int>>> new_vertex_combinations;
+    vector<vector<int>> new_vertex_combinations;
+    vector<int> new_vertex_combination, new_edges_combination;
+    vector<vector<int>> graph;
+    graph.push_back(new_line);
+    int* delta_U = new int;
+    *delta_U = new_line.size();
+    for (int level_num = 0; level_num < new_line.size(); level_num++) {
+        int p = level_num - 1;
+        vector<int> v;
+        for (int t = 0; t < level_num; t++) v.push_back(t);
+        while (p >= 0) {
+            vector<int> Xu;
+            for (int j = 0; j < level_num; j++) Xu.push_back(new_line[v[j]]);
+            new_vertex_combination = g(old_graph, Xu);
+            bool flag = 1;
+            for (int i = 0; i < new_vertex_combinations.size(); i++) {
+                if (new_vertex_combinations[i] == new_vertex_combination) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) {
+                new_edges_combination = g(old_graph, new_vertex_combination, false);
+                bool flag = 1;
+                for (int j = 0; j < MCS.size(); j++) {
+                    if (MCS[j].first == new_edges_combination) {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag) {
+                    MCS.push_back(make_pair(new_edges_combination, new_vertex_combination));
+                }
+            }
+            if (v[level_num - 1] == delta_U[level_num] - 1) p--;
+            else p = level_num - 1;
+            if (p >= 0) for (int j = level_num - 1; j >= p; j--) v[j] = v[p] + j - p + 1;
+        }
+    }
+}
 
- int* delta_U = new int;
- *delta_U = new_line.size();
- for (int level_num = 0; level_num < new_line.size(); level_num++) {
-  int p = level_num - 1;
-  vector<int> v;
-  for (int t = 0; t < level_num; t++) v.push_back(t);
-  while (p >= 0) {
-   vector<int> Xu;
-   for (int j = 0; j < level_num; j++) Xu.push_back(new_line[level_num]);
-   new_vertex_combinations.push_back(Xu);
-   if()
-   if (v[level_num - 1] == delta_U[level_num] - 1) p--;
-   else p = level_num - 1;
-   if (p >= 0) for (int j = level_num - 1; j >= p; j--) v[j] = v[p] + j - p + 1;
-  }
- }
- /*
- HFindMCS(new_vertex_combinations, graph);
- for (int i = 0; i < new_vertex_combinations.size(); i++) {
-  bool flag = 0;
-  vector<int> C = gg(old_Graph, new_vertex_combinations[i].first, true);      //хз что это, не помню
-  for (int j = 0; j < MCS.size(); j++) {
-   if (MCS[j].first == C) {
-    MCS[j].second.push_back(old_Graph.size());
-    flag = 1;
-    break;
-   }
-  }
-  if (!flag) {
-   vector<int>D = g(old_Graph, new_vertex_combinations[i].first, true);
-   MCS.push_back(pair<vector<int>, vector<int>>(C, D));
-  }
- }*/
- //}
 void Graph_output(vector<vector<int>> Graph) { // работает правильно
     int max_edge = 0;
     for (int i = 0; i < Graph.size(); i++) if (Graph[i][Graph[i].size() - 1] > max_edge) max_edge = Graph[i][Graph[i].size() - 1];
@@ -237,25 +253,12 @@ void Graph_output(vector<vector<int>> Graph) { // работает правильно
         cout << endl;
     }
 }
+
 int main() {
     srand(time(0));
+    vector<vector<int>> graph, new_graph;
+    const int N = 5, M = 5, delta = 5;
 
-    /*vector<vector<int>> graph(6), graph2;
-    graph[0].push_back(0);
-    graph[1].push_back(0);
-    graph[1].push_back(1);
-    graph[2].push_back(0);
-    graph[2].push_back(1);
-    graph[2].push_back(2);
-    graph[3].push_back(3);
-    graph[4].push_back(2);
-    graph[5].push_back(2);
-    graph2 = graph;
-    Graph_output(Graph_Transpose(graph));*/
-
-    vector<vector<int>> graph;
-    const int N = 1000, M = 1000, delta = 10;
-    //cout << N / delta * 2 + 1;
     for (int i = 0; i < N; i++) {                                //рандомная генерация графа
         graph.push_back(vector<int>());
         int k = 0;
@@ -267,41 +270,38 @@ int main() {
         }
         if (k == 0) graph[i].push_back(rand() % N);
     }
-    cout << "nahalo" << endl;
-    //Graph_output(graph);
-    map<vector<int>, vector<int>> MCS1;
-    map<vector<int>, vector<int>> MCS2;
+
+    new_graph = graph;
+    vector<int> new_vertex;
+    int k = 0;
+
+    for (int i = 0; i < M; i++) {
+        if (rand() % (N / delta + 1) == 0 && k < delta) {
+            new_vertex.push_back(i);
+            k++;
+        }
+    }
+
+    new_graph.push_back(new_vertex);
+    cout << "na4alo" << endl;
+    Graph_output(graph);
+    cout << endl;
+    int k = 0;
+    for (int j = 0; j < new_vertex.size(); j++) {
+        while (k++ < new_vertex[j]) cout << 0 << " ";
+        cout << 1 << " ";
+    }
+    while (k++ <= M) cout << 0 << " ";
+
+    //Graph_output(Graph_Transpose(graph));
+    vector<pair<vector<int>, vector<int>>> MCS1;
+    vector<pair<vector<int>, vector<int>>> MCS2;
     int time = omp_get_wtime();
     HFindMCS(MCS1, graph);
+    Dynamic_MCS(MCS1, new_vertex, graph);
     cout << endl << omp_get_wtime() - time << endl << MCS1.size();
     time = omp_get_wtime();
-    HFindMCS_parallel(MCS2, graph);
+    //HFindMCS_parallel(MCS2, graph);
+    HFindMCS(MCS2, new_graph);
     cout << endl << omp_get_wtime() - time << endl << MCS1.size();
-    if (MCS1==MCS2) cout << endl << "DA HY HAXYU";
-    //Dynamic_MCS(MCS, a, graph);
-    /*
-    for (auto p = MCS1.begin(); p != MCS1.end(); p++) {
-        cout << endl << "C:";
-        for (int j = 0; j < p->first.size(); j++) {     //Вывод MCS
-            cout << p->first[j] << ' ';
-        }
-        cout << endl << "D:";
-        for (int j = 0; j < p->second.size(); j++) {
-            cout << p->second[j] << ' ';
-        }
-        cout << endl;
-    }
-    cout << "_____________________________" << endl;
-    for (auto p2 = MCS2.begin(); p2 != MCS2.end(); p2++) {
-        cout << endl << "C:";
-        for (int j = 0; j < p2->first.size(); j++) {     //Вывод MCS
-            cout << p2->first[j] << ' ';
-        }
-        cout << endl << "D:";
-        for (int j = 0; j < p2->second.size(); j++) {
-            cout << p2->second[j] << ' ';
-        }
-        cout << endl;
-    }
-    //*/
 }
